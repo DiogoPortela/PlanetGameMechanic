9f107cbad5badc45e0957ea1b0910c4a42ce63fb
Diogo Portela <diogo.ribeiro.portela@gmail.com>
2019-12-06 20:36:09 +0000

    Tutorial 3 adding Simple Noise, and multiple filters

---
+++ Assets/Scripts/Planets/Settings/NoiseSettings.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class NoiseSettings
+{
+    public float strenght = 1;
+    [Range(1,8)]
+    public int numLayers = 1;
+    public float baseRoughtness = 1;
+    public float roughness = 2;
+    public float persistance = .5f;
+    public Vector3 centre;
+    public float minValue;
+}
