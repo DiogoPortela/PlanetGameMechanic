bf2a0c961fde8d8d5f113b18ce9b15c94414f156
Diogo Portela <diogo.ribeiro.portela@gmail.com>
2019-12-07 16:04:00 +0000

    Tutorial 4 Adding more noises and only render one face.

--- Assets/Scripts/Planets/PlanetIcosphere.cs
+++ Assets/Scripts/Planets/PlanetIcosphere.cs
@@ -8,6 +8,8 @@ public class PlanetIcosphere : MonoBehaviour
     [Range(1, 8)]
     public int resolution = 1;
     public bool autoUpdate = true;
+    public enum FaceRenderMask { All, one }
+    public FaceRenderMask faceRenderMask;
 
     public ShapeSettings shapeSettings;
     public ColorSettings colorSettings;
@@ -51,6 +53,8 @@ public class PlanetIcosphere : MonoBehaviour
             }
 
             terrainFaces[i] = new TerrainFaceTriangle(shapeGenerator, meshFilters[i].sharedMesh, resolution);
+            bool renderFace = faceRenderMask == FaceRenderMask.All || (int)faceRenderMask - 1 == i;
+            meshFilters[i].gameObject.SetActive(renderFace);
         }
     }
     public void GeneratePlanet()
@@ -97,28 +101,31 @@ public class PlanetIcosphere : MonoBehaviour
         icoVertices.Add(new Vector3(-t, 0, 1));
 
         terrainFaces[0].ConstructMesh(icoVertices[0], icoVertices[11], icoVertices[5]);
-        terrainFaces[1].ConstructMesh(icoVertices[0], icoVertices[5], icoVertices[1]);
-        terrainFaces[2].ConstructMesh(icoVertices[0], icoVertices[1], icoVertices[7]);
-        terrainFaces[3].ConstructMesh(icoVertices[0], icoVertices[7], icoVertices[10]);
-        terrainFaces[4].ConstructMesh(icoVertices[0], icoVertices[10], icoVertices[11]);
+        if (faceRenderMask == FaceRenderMask.All)
+        {
+            terrainFaces[1].ConstructMesh(icoVertices[0], icoVertices[5], icoVertices[1]);
+            terrainFaces[2].ConstructMesh(icoVertices[0], icoVertices[1], icoVertices[7]);
+            terrainFaces[3].ConstructMesh(icoVertices[0], icoVertices[7], icoVertices[10]);
+            terrainFaces[4].ConstructMesh(icoVertices[0], icoVertices[10], icoVertices[11]);
 
-        terrainFaces[5].ConstructMesh(icoVertices[1], icoVertices[5], icoVertices[9]);
-        terrainFaces[6].ConstructMesh(icoVertices[5], icoVertices[11], icoVertices[4]);
-        terrainFaces[7].ConstructMesh(icoVertices[11], icoVertices[10], icoVertices[2]);
-        terrainFaces[8].ConstructMesh(icoVertices[10], icoVertices[7], icoVertices[6]);
-        terrainFaces[9].ConstructMesh(icoVertices[7], icoVertices[1], icoVertices[8]);
+            terrainFaces[5].ConstructMesh(icoVertices[1], icoVertices[5], icoVertices[9]);
+            terrainFaces[6].ConstructMesh(icoVertices[5], icoVertices[11], icoVertices[4]);
+            terrainFaces[7].ConstructMesh(icoVertices[11], icoVertices[10], icoVertices[2]);
+            terrainFaces[8].ConstructMesh(icoVertices[10], icoVertices[7], icoVertices[6]);
+            terrainFaces[9].ConstructMesh(icoVertices[7], icoVertices[1], icoVertices[8]);
 
-        terrainFaces[10].ConstructMesh(icoVertices[3], icoVertices[9], icoVertices[4]);
-        terrainFaces[11].ConstructMesh(icoVertices[3], icoVertices[4], icoVertices[2]);
-        terrainFaces[12].ConstructMesh(icoVertices[3], icoVertices[2], icoVertices[6]);
-        terrainFaces[13].ConstructMesh(icoVertices[3], icoVertices[6], icoVertices[8]);
-        terrainFaces[14].ConstructMesh(icoVertices[3], icoVertices[8], icoVertices[9]);
+            terrainFaces[10].ConstructMesh(icoVertices[3], icoVertices[9], icoVertices[4]);
+            terrainFaces[11].ConstructMesh(icoVertices[3], icoVertices[4], icoVertices[2]);
+            terrainFaces[12].ConstructMesh(icoVertices[3], icoVertices[2], icoVertices[6]);
+            terrainFaces[13].ConstructMesh(icoVertices[3], icoVertices[6], icoVertices[8]);
+            terrainFaces[14].ConstructMesh(icoVertices[3], icoVertices[8], icoVertices[9]);
 
-        terrainFaces[15].ConstructMesh(icoVertices[4], icoVertices[9], icoVertices[5]);
-        terrainFaces[16].ConstructMesh(icoVertices[2], icoVertices[4], icoVertices[11]);
-        terrainFaces[17].ConstructMesh(icoVertices[6], icoVertices[2], icoVertices[10]);
-        terrainFaces[18].ConstructMesh(icoVertices[8], icoVertices[6], icoVertices[7]);
-        terrainFaces[19].ConstructMesh(icoVertices[9], icoVertices[8], icoVertices[1]);
+            terrainFaces[15].ConstructMesh(icoVertices[4], icoVertices[9], icoVertices[5]);
+            terrainFaces[16].ConstructMesh(icoVertices[2], icoVertices[4], icoVertices[11]);
+            terrainFaces[17].ConstructMesh(icoVertices[6], icoVertices[2], icoVertices[10]);
+            terrainFaces[18].ConstructMesh(icoVertices[8], icoVertices[6], icoVertices[7]);
+            terrainFaces[19].ConstructMesh(icoVertices[9], icoVertices[8], icoVertices[1]);
+        }
 
     }
     void GenerateColors()
--- Assets/Scripts/Planets/Tutorial/PlanetCube.cs
+++ Assets/Scripts/Planets/Tutorial/PlanetCube.cs
@@ -8,6 +8,9 @@ public class PlanetCube : MonoBehaviour
     public int resolution = 1;
     public bool autoUpdate = true;
 
+    public enum FaceSquareRenderMask { All, Top, Bottom, Left, Right, Front, Back }
+    public FaceSquareRenderMask faceRenderMask;
+
     public ShapeSettings shapeSettings;
     public ColorSettings colorSettings;
 
@@ -51,6 +54,8 @@ public class PlanetCube : MonoBehaviour
             }
 
             terrainFaces[i] = new TerrainFaceSquare(shapeGenerator, meshFilters[i].sharedMesh, resolution, directions[i]);
+            bool renderFace = faceRenderMask == FaceSquareRenderMask.All || (int)faceRenderMask - 1 == i;
+            meshFilters[i].gameObject.SetActive(renderFace);
         }
     }
     public void GeneratePlanet()
@@ -80,9 +85,13 @@ public class PlanetCube : MonoBehaviour
 
     void GenerateMesh()
     {
-        foreach (var face in terrainFaces)
+        for (int i = 0; i < 6; i++)
         {
-            face.ConstructMesh();
+            if (meshFilters[i].gameObject.activeSelf)
+            {
+                terrainFaces[i].ConstructMesh();
+
+            }
         }
     }
 
